f160646c6dae049b11b0410de36f1cbf187340d3
igorbelow <lord.below@yandex.ru>
2023-03-11 16:15:18 +0300

    RecipeBook.

--- src/main/java/info/localhost/recipebook/controllers/RecipeController.java
+++ src/main/java/info/localhost/recipebook/controllers/RecipeController.java
@@ -2,15 +2,35 @@ package info.localhost.recipebook.controllers;
 
 
 import info.localhost.recipebook.model.Recipe;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.support.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+import static info.localhost.recipebook.controllers.IngredientController.getStringStringMap;
+
 @RestController
 @RequestMapping("/recipe")
 @RequiredArgsConstructor
+@Tag(name = "рецепты",description = "CRUD- операции для работы с рецептами")
 public class RecipeController {
     private final RecipeController recipeService;
+    @Operation(summary = "Поиск рецептов по id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "рецепт был обнаружен")})
+    @Parameters(value = {@Parameter(name = "id",example = "1")})
     @GetMapping("/{id}")
     public ResponseEntity<Recipe> getRecipe(@PathVariable Integer id) {
         Recipe recipe = recipeService.getRecipe(id).getBody();
@@ -21,12 +41,31 @@ public class RecipeController {
     }
 
     @PostMapping
+    @Operation(summary = "Получение всех рецептов", description = "Поиск производится без параметров")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200",description = "рецепт добавлен")})
     public ResponseEntity<ResponseEntity<Long>> addRecipe(@Valid @RequestBody Recipe recipe) {
         ResponseEntity<Long> id = recipeService.addRecipe(recipe).getBody();
         return ResponseEntity.ok(id);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "изменение рецептов по id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Рецепт изменен",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = Recipe.class)
+                            )
+                    }
+            )
+    })
+    @Parameters(value = {
+            @Parameter(name = "id",
+            example = "1")
+    })
     public ResponseEntity<Object> addRecipe(@PathVariable long id, @RequestBody ResponseEntity<Object> recipe) {
         recipe = recipeService.addRecipe(id, recipe);
         if (recipe == null) {
@@ -36,6 +75,14 @@ public class RecipeController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "удаление рецептов по id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "рецепт удален"
+            )
+    })
+    @Parameters(value = {@Parameter(name = "id",example = "1")})
     public boolean removeRecipe(@PathVariable Integer id){
         if (recipeService.removeRecipe(id)) {
             return ResponseEntity.ok().build().hasBody();
@@ -43,11 +90,19 @@ public class RecipeController {
         return ResponseEntity.notFound().build().hasBody();
     }
     @DeleteMapping
+    @Operation(summary = "удалени всех рецептов", description = "Удаление производится без параметров")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200",description = "рецепты удалены")})
     public ResponseEntity<Void>removeAllRecipe(){
         recipeService.removeAllRecipe();
         return ResponseEntity.ok().build();
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String , String> handValidationException(
+            MethodArgumentNotValidException e) {
+        return getStringStringMap(e);
+    }
 
 }
 
